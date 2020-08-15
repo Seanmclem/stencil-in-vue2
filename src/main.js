1e5c29b976531dcd16a99c9565f7d425217ed0ea
seanmclem <seanmclements@gmail.com>
2020-08-14 21:10:55 -0400

    adding updating lib, remove helloWorld

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { defineCustomElements } from 'site-switcher/loader'
+import { defineCustomElements } from 'test-demo-seanmclem/loader'
 
 Vue.config.productionTip = false
-
-defineCustomElements()
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+defineCustomElements()
