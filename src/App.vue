3d4c1ee8af722c318a3c3bc9d109370dda1061b2
seanmclem <seanmclements@gmail.com>
2020-08-14 20:56:05 -0400

    added my-component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-component first="Your" middle="Full" last="Name"></my-component>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { defineCustomElements } from 'site-switcher/loader'
 
 Vue.config.productionTip = false
+
+defineCustomElements()
 
 new Vue({
   render: h => h(App),
